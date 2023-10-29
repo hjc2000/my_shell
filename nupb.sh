125651f9d88e6c36b08a2a47963d422732eb2b95
hjc2000 <huangjincanqqmail@qq.com>
2023-10-29 23:56:29 +0800

    1

--- nupb.sh
+++ nupb.sh
@@ -1,13 +1,17 @@
 # 获取最近一条提交记录
 log=$(git log -1)
-# 保存工作目录到变量
+
+# 保存当前工作目录到变量
 work_dir=$(pwd)
-# 先拉取
+
+# 先切换到包源，进行拉取
 cd $NUGET
 git pull
+
 # 回到工作目录，发布新包
 cd $work_dir
 nuget.exe init ./ $NUGET
+
 # 切到包源并推动到 github
 cd $NUGET
 git add .
