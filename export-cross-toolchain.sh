58e3a4ce31017880fde3f51665fe2d45de9704dc
hjc2000 <huangjincanqqmail@qq.com>
2024-02-15 00:52:17 +0800

    删除无用脚本

--- export-cross-toolchain.sh
+++
@@ -1,6 +0,0 @@
-export CC=arm-none-linux-gnueabihf-gcc
-export CXX=arm-none-linux-gnueabihf-g++
-export LD=arm-none-linux-gnueabihf-ld
-export AR=arm-none-linux-gnueabihf-ar
-export AS=arm-none-linux-gnueabihf-as
-export RANLIB=arm-none-linux-gnueabihf-ranlib
