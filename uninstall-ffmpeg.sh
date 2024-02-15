3b4f6ee9231245065177850779c7db78a1e46161
hjc2000 <huangjincanqqmail@qq.com>
2024-02-15 13:08:48 +0800

    auto commit

--- uninstall-ffmpeg.sh
+++ uninstall-ffmpeg.sh
@@ -1,5 +1,23 @@
 root_path=$(pwd)
 
 cd lib &&
-rm -rf libx265.* libx264.* libswscale.* libswresample.so.* libavcodec.* libavdevice.* \
+rm -rf \
+libx265.* \
+libx264.* \
+libswscale.* \
+libswresample.so.* \
+libavcodec.* \
+libavdevice.* \
+libavfilter.* \
+libavformat.* \
+libavutil.* \
+libpostproc.* 
 
+
+cd ${root_path} &&
+cd bin &&
+rm -rf \
+ffmpeg \
+ffprobe \
+x264 \
+x265
