e2c2254e0a1a0f0241f08ae884668664fa0eb9ab
hjc2000 <huangjincanqqmail@qq.com>
2024-02-21 15:25:25 +0800

    1

--- install-to-usr.sh
+++ install-to-usr.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# 确保脚本以 root 权限执行
-if [ "$(id -u)" != "0" ]; then
-   echo "该脚本必须以 root 权限运行" 1>&2
-   exit 1
-fi
-
 # 检查是否有通过管道传递的输入
 if [ -p /dev/stdin ]; then
     # 如果有管道输入，则从标准输入读取目录名
