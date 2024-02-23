46cc3ef94c7942f984e33a85a0b80789aa183b02
hjc2000 <huangjincanqqmail@qq.com>
2024-02-23 15:48:30 +0800

    1

--- msbuild.ps1
+++ msbuild.ps1
@@ -1,15 +1,7 @@
-# 定义枚举类型
-Add-Type -TypeDefinition @"
-    public enum BuildConfiguration {
-        Debug,
-        Release
-    }
-"@
-
-# 使用param定义参数，包括使用枚举类型的参数
 param (
-	[BuildConfiguration]$Configuration,
-	[string]$SolutionPath
+	[switch]$Debug,
+	[string]$SolutionPath,
+	[string]$Platform = "x64" # 默认平台为x64，但用户可以通过参数覆盖
 )
 
 # 检查解决方案路径是否有效
@@ -19,7 +11,27 @@ if (-Not (Test-Path $SolutionPath))
 	exit
 }
 
+# 根据 $Debug 开关设置构建配置
+$configuration = if ($Debug) { "Debug" } else { "Release" }
+
 # 根据参数执行逻辑
-Write-Host "开始构建 $($Configuration) 配置..."
-msbuild $SolutionPath /p:Configuration=$Configuration /p:Platform="Any CPU"
-Write-Host "构建完成。"
+Write-Host "开始构建 $($configuration) 配置，平台: $($Platform)..."
+MSBuild.exe $SolutionPath /p:Configuration=$configuration /p:Platform=$Platform
+Write-Host "主解决方案构建完成。"
+
+# 构建 INSTALL 项目
+# 获取解决方案所在的目录
+$solutionDir = Split-Path -Parent $SolutionPath
+# 定义 INSTALL 项目文件的路径
+$installProjectPath = Join-Path $solutionDir "INSTALL.vcxproj"
+
+if (Test-Path $installProjectPath)
+{
+	Write-Host "开始构建 INSTALL 项目..."
+	MSBuild.exe $installProjectPath /p:Configuration=$configuration /p:Platform=$Platform
+	Write-Host "INSTALL 项目构建完成。"
+}
+else
+{
+	Write-Host "未找到 INSTALL 项目，跳过此步骤。"
+}
