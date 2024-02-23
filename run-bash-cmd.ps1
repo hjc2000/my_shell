96b9082a6b8957cff59529a3a7dbc4ea31f82e56
hjc2000 <huangjincanqqmail@qq.com>
2024-02-23 14:15:20 +0800

    "|| $cmd" | bash -himBHs

--- run-bash-cmd.ps1
+++ run-bash-cmd.ps1
@@ -4,6 +4,18 @@ param (
 )
 $ErrorActionPreference = "Stop"
 
-$cmd.Trim()
 $env:MSYSTEM = "UCRT64"
-bash -c "${cmd} && exit"
+$env:MSYS2_PATH_TYPE = "inherit"
+$env:MSYSCON = "defterm"
+$env:MSYSTEM_PREFIX = "/ucrt64"
+$env:MINGW_PREFIX = "/ucrt64"
+$env:MSYSTEM_CHOST = "x86_64-w64-mingw32"
+$env:MSYSTEM_CARCH = "x86_64"
+$env:CHERE_INVOKING = "enabled_from_arguments"
+$env:MSYS2_NOSTART = "yes"
+$env:CONTITLE = "MinGW UCRT x64"
+
+$cmd = $cmd.Trim()
+$cmd = $cmd.Replace("`t", " ")
+$cmd = $cmd.Replace("`r`n", "`n")
+"|| $cmd" | bash -himBHs
