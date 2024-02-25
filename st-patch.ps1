c1f33e150344cb4e35fe0eb3d4ffe29064504bc7
hjc2000 <huangjincanqqmail@qq.com>
2024-02-25 15:21:07 +0800

    1

--- st-patch.ps1
+++ st-patch.ps1
@@ -1,7 +1,11 @@
-#!/bin/bash
-
 # 用来给 st 公司或正点原子的 linux SDK 打补丁。进入到源码目录，
 # 源码目录的上层目录有一个 patch 文件，这就是补丁文件。
 # 在源码目录中执行本脚本，即可打补丁。
+$patchFiles = Get-ChildItem -Path "../*.patch"
 
-for p in `ls -1 ../*.patch`; do patch -p1 < $p; done
+# 遍历每个文件
+foreach ($file in $patchFiles)
+{
+	# 应用patch
+	patch -p1 -i $file.FullName
+}
