5ee85f785f3516fd1e71337a6455dc7b9b9526f5
hjc2000 <huangjincanqqmail@qq.com>
2024-02-26 16:25:18 +0800

    auto commit

--- install-lib.ps1
+++ install-lib.ps1
@@ -13,14 +13,14 @@ New-Item -Path $dst_path/bin/ -ItemType Directory -Force
 New-Item -Path $dst_path/lib/ -ItemType Directory -Force
 New-Item -Path $dst_path/include/ -ItemType Directory -Force
 
-Copy-Item -Path $src_path/bin/ `
+Copy-Item -Path $src_path/bin/* `
 	-Destination $dst_path/bin/ `
 	-Force -Recurse
 
-Copy-Item -Path $src_path/lib/ `
+Copy-Item -Path $src_path/lib/* `
 	-Destination $dst_path/lib/ `
 	-Force -Recurse
 
-Copy-Item -Path $src_path/include/ `
+Copy-Item -Path $src_path/include/* `
 	-Destination $dst_path/include/ `
 	-Force -Recurse
