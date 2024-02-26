7040e2e1a69825ff3cc7347902543660126f63c8
hjc2000 <huangjincanqqmail@qq.com>
2024-02-26 16:59:30 +0800

    auto commit

--- install-lib.ps1
+++ install-lib.ps1
@@ -15,6 +15,8 @@ New-Item -Path $dst_path/include/ -ItemType Directory -Force
 
 run-bash-cmd.ps1 @"
 set -e
+
+# 复制并保留符号链接
 cp -a -r $src_path/bin/* $dst_path/bin/
 cp -a -r $src_path/lib/* $dst_path/lib/
 cp -a -r $src_path/include/* $dst_path/include/
