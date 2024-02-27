06caaafd7ac9a05ac2d95bfbaf767001d7d68a34
hjc2000 <huangjincanqqmail@qq.com>
2024-02-27 08:34:12 +0800

    install-lib.ps1

--- install-lib.ps1
+++ install-lib.ps1
@@ -13,11 +13,41 @@ New-Item -Path $dst_path/bin/ -ItemType Directory -Force
 New-Item -Path $dst_path/lib/ -ItemType Directory -Force
 New-Item -Path $dst_path/include/ -ItemType Directory -Force
 
-run-bash-cmd.ps1 @"
+if ($(is-msys.ps1))
+{
+	Copy-Item -Path "$src_path/bin/*" -Destination "$dst_path/bin/" -Force -Recurse
+	if (Test-Path "$src_path/lib/*")
+	{
+		Copy-Item -Path "$src_path/lib/*" -Destination "$dst_path/lib/" -Force -Recurse
+	}
+	elseif (Test-Path -Path "$src_path/lib64")
+	{
+		Copy-Item -Path "$src_path/lib64/*" -Destination "$dst_path/lib/" -Force -Recurse
+	}
+	elseif (Test-Path -Path "$src_path/lib32")
+	{
+		Copy-Item -Path "$src_path/lib32/*" -Destination "$dst_path/lib/" -Force -Recurse
+	}
+
+
+	Copy-Item -Path "$src_path/include/*" -Destination "$dst_path/include/" -Force -Recurse
+}
+else
+{
+	run-bash-cmd.ps1 @"
 set -e
 
 # 复制并保留符号链接
 cp -a -r $src_path/bin/* $dst_path/bin/
-cp -a -r $src_path/lib/* $dst_path/lib/
+
+if [ -d "$src_path/lib" ]; then
+  cp -a -r "$src_path/lib/"* "$dst_path/lib/"
+elif [ -d "$src_path/lib64" ]; then
+  cp -a -r "$src_path/lib64/"* "$dst_path/lib/"
+elif [ -d "$src_path/lib32" ]; then
+  cp -a -r "$src_path/lib32/"* "$dst_path/lib/"
+fi
+
 cp -a -r $src_path/include/* $dst_path/include/
 "@
+}
