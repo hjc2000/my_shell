d67c482bbf72442d3554e75efeecf1ebdea0fcfd
hjc2000 <huangjincanqqmail@qq.com>
2024-03-06 14:37:48 +0800

    auto commit

--- wget-repo.ps1
+++ wget-repo.ps1
@@ -37,7 +37,8 @@ if (Test-Path -Path $out_dir_name)
 # 解压后的目录不存在，检查压缩包是否存在
 if (-not (Test-Path -Path $file_name))
 {
-	Invoke-WebRequest -Uri $repo_url -OutFile $file_name
+	# Invoke-WebRequest -Uri $repo_url -OutFile $file_name
+	wget $repo_url -O $file_name
 }
 
 # 创建输出目录
