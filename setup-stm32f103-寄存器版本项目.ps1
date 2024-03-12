ece7fb7c6312507dcc84af1348ac014ac15ac331
hjc2000 <huangjincanqqmail@qq.com>
2024-03-12 23:21:30 +0800

    auto commit

--- setup-stm32f103-寄存器版本项目.ps1
+++ setup-stm32f103-寄存器版本项目.ps1
@@ -5,8 +5,11 @@ param (
 $ErrorActionPreference = "Stop"
 
 $project_full_path = "$(Get-Location)/$project_name"
-New-Item -ItemType Directory -Force -Path "$project_full_path" 				
+New-Item -ItemType Directory -Force -Path "$project_full_path"
 New-Item -ItemType Directory -Force -Path "$project_full_path/Drivers"
+New-Item -ItemType Directory -Force -Path "$project_full_path/Drivers/BSP"
+New-Item -ItemType Directory -Force -Path "$project_full_path/Drivers/CMSIS"
+New-Item -ItemType Directory -Force -Path "$project_full_path/Drivers/SYSTEM"
 New-Item -ItemType Directory -Force -Path "$project_full_path/Middlewares"
 New-Item -ItemType Directory -Force -Path "$project_full_path/Output"
 New-Item -ItemType Directory -Force -Path "$project_full_path/Projects"
