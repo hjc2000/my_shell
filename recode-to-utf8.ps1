fd99456f3922ade38d8a5c01a84afc7d7fafca5d
hjc2000 <huangjincanqqmail@qq.com>
2024-03-13 10:53:06 +0800

    auto commit

--- recode-to-utf8.ps1
+++ recode-to-utf8.ps1
@@ -6,7 +6,7 @@ param (
 )
 $ErrorActionPreference = "Stop"
 
-iconv -f $(uchardet $input_file_path) -t UTF-8 $input_file_path > $output_file_path | Out-Null
+iconv -f $(uchardet $input_file_path) -t UTF-8 $input_file_path > $output_file_path
 if (-not $LASTEXITCODE)
 {
 	Write-Host "转换成功"
