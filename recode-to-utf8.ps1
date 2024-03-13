81ec397660c19d94d5bf9ce4ceb54169dd0668aa
hjc2000 <huangjincanqqmail@qq.com>
2024-03-14 00:17:24 +0800

    auto commit

--- recode-to-utf8.ps1
+++ recode-to-utf8.ps1
@@ -1,23 +1,23 @@
 param (
 	[Parameter(Mandatory = $true)]
-	[string]$input_file_path,
-	[Parameter(Mandatory = $true)]
-	[string]$output_file_path
+	[string]$input_file_path
 )
 $ErrorActionPreference = "Stop"
 
-iconv -f $(uchardet $input_file_path) -t UTF-8 $input_file_path > $output_file_path
+$recode_result = iconv -f $(uchardet $input_file_path) -t UTF-8 $input_file_path
 if (-not $LASTEXITCODE)
 {
 	Write-Host "转换成功"
+	$recode_result | Set-Content -Path $input_file_path
 	return
 }
 
 Write-Host "转换失败，猜测原始文件为 GB2312，再次尝试转换"
-iconv -f GB2312 -t UTF-8 $input_file_path > $output_file_path
+$recode_result = iconv -f GB2312 -t UTF-8 $input_file_path
 if (-not $LASTEXITCODE)
 {
 	Write-Host "转换成功"
+	$recode_result | Set-Content -Path $input_file_path
 	return
 }
 
