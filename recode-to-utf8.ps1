88686e868a89291fc532413f64fef09f3f6f6c3e
hjc2000 <huangjincanqqmail@qq.com>
2024-03-13 10:51:34 +0800

    auto commit

--- recode-to-utf8.ps1
+++ recode-to-utf8.ps1
@@ -6,4 +6,19 @@ param (
 )
 $ErrorActionPreference = "Stop"
 
-iconv -f $(uchardet $input_file_path) -t UTF-8 $input_file_path > $output_file_path
+iconv -f $(uchardet $input_file_path) -t UTF-8 $input_file_path > $output_file_path | Out-Null
+if (-not $LASTEXITCODE)
+{
+	Write-Host "转换成功"
+	return
+}
+
+Write-Host "转换失败，猜测原始文件为 GB2312，再次尝试转换"
+iconv -f GB2312 -t UTF-8 $input_file_path > $output_file_path
+if (-not $LASTEXITCODE)
+{
+	Write-Host "转换成功"
+	return
+}
+
+Write-Host "转换失败"
