6c23ce63f1efce09f3e081b8ac471e9f64657e7b
hjc2000 <huangjincanqqmail@qq.com>
2024-03-14 10:49:22 +0800

    auto commit

---
+++ recode-c-h-to-utf8.ps1
@@ -0,0 +1,10 @@
+param (
+	[Parameter(Mandatory = $true)]
+	[string]$input_file_path
+)
+
+recode-to-utf8.ps1 -input_file_path "$input_file_path/*.h" -Recurse
+recode-to-utf8.ps1 -input_file_path "$input_file_path/*.c" -Recurse
+recode-to-utf8.ps1 -input_file_path "$input_file_path/*.cpp" -Recurse
+recode-to-utf8.ps1 -input_file_path "$input_file_path/*.js" -Recurse
+recode-to-utf8.ps1 -input_file_path "$input_file_path/*.html" -Recurse
