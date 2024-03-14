8b7bae0f6323b4b018f9cac2e1280ee95a2c87cc
hjc2000 <huangjincanqqmail@qq.com>
2024-03-14 10:55:19 +0800

    auto commit

--- recode-source-code-file-to-utf8.ps1
+++ recode-source-code-file-to-utf8.ps1
@@ -6,8 +6,11 @@ param (
 recode-to-utf8.ps1 -input_file_path "$input_file_path/*.h" -Recurse
 recode-to-utf8.ps1 -input_file_path "$input_file_path/*.c" -Recurse
 recode-to-utf8.ps1 -input_file_path "$input_file_path/*.cpp" -Recurse
+recode-to-utf8.ps1 -input_file_path "$input_file_path/*.hpp" -Recurse
 recode-to-utf8.ps1 -input_file_path "$input_file_path/*.js" -Recurse
 recode-to-utf8.ps1 -input_file_path "$input_file_path/*.html" -Recurse
+recode-to-utf8.ps1 -input_file_path "$input_file_path/*.json" -Recurse
+recode-to-utf8.ps1 -input_file_path "$input_file_path/*.xml" -Recurse
 recode-to-utf8.ps1 -input_file_path "$input_file_path/*.txt" -Recurse
 recode-to-utf8.ps1 -input_file_path "$input_file_path/*.s" -Recurse
 recode-to-utf8.ps1 -input_file_path "$input_file_path/*.py" -Recurse
