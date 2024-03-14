1e840327b86cd8987a3bd3152afa32494966e6ad
hjc2000 <huangjincanqqmail@qq.com>
2024-03-14 10:37:30 +0800

    auto commit

--- recode-to-utf8.ps1
+++ recode-to-utf8.ps1
@@ -16,14 +16,5 @@ foreach ($file in $file_list)
 		continue
 	}
 	
-	Write-Host "转换失败，猜测原始文件为 GB18030, 再次尝试转换"
-	$conv_result = iconv -f GB18030 -t UTF-8 $file.FullName
-	if (-not $LASTEXITCODE)
-	{
-		Write-Host "$($file.FullName) 转换成功"
-		$conv_result | Out-File $file.FullName
-		continue
-	}
-	
-	Write-Host "$($file.FullName) 转换失败"
+	Write-Error "$($file.FullName) 转换失败"
 }
