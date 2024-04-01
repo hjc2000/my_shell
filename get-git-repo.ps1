ca5f407adc6218823831a25dceca918e31c6a39d
hjc2000 <huangjincanqqmail@qq.com>
2024-04-01 13:19:07 +0800

    Push-Location

--- get-git-repo.ps1
+++ get-git-repo.ps1
@@ -8,41 +8,52 @@ $ErrorActionPreference = "Stop"
 git config --global submodule.recurse true
 $repoName = & parse-git-repo-name.ps1 -git_url $git_url
 
-if (Test-Path "./$repoName/")
+Push-Location
+try
 {
-	$current_path = Get-Location
-	Set-Location $repoName
-	if ($branch_name)
+	if (Test-Path "./$repoName/")
 	{
-		git pull origin $branch_name
-		git checkout $branch_name
-	}
-	else
-	{
-		git pull
-	}
-	
-	Set-Location $current_path
-}
-else
-{
-	while ($true)
-	{
+		# 已经存在该仓库的文件夹
+		Set-Location $repoName
 		if (-not [string]::IsNullOrEmpty($branch_name))
 		{
-			git clone --branch $branch_name $git_url
+			git pull origin $branch_name
+			git checkout $branch_name
 		}
 		else
 		{
-			git clone $git_url
+			git pull
 		}
-
-		if ($?)
-		{
-			# 如果上一个命令成功，则退出循环
-			break 
-		} 
-		Write-Host "Clone failed, retrying..."
-		Start-Sleep -Seconds 5
 	}
+	else
+	{
+		# 不存在该仓库的文件夹，需要克隆
+		while ($true)
+		{
+			if (-not [string]::IsNullOrEmpty($branch_name))
+			{
+				git clone --branch $branch_name $git_url
+			}
+			else
+			{
+				git clone $git_url
+			}
+	
+			if ($?)
+			{
+				# 如果上一个命令成功，则退出循环
+				break 
+			} 
+			
+			Write-Host "Clone failed, retrying..."
+			Start-Sleep -Seconds 5
+		}
+	}
+}
+catch
+{
+}
+finally
+{
+	Pop-Location
 }
