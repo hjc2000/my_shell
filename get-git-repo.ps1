27d28c2639efe263edacf46d977052907833553f
hjc2000 <huangjincanqqmail@qq.com>
2024-04-02 18:04:26 +0800

    Push-Location

--- get-git-repo.ps1
+++ get-git-repo.ps1
@@ -6,25 +6,24 @@ param (
 $ErrorActionPreference = "Stop"
 
 $repoName = & parse-git-repo-name.ps1 -git_url $git_url
-Push-Location
-try
+while ($true)
 {
-	while ($true)
+	if (Test-Path "./$repoName/")
 	{
-		if (Test-Path "./$repoName/")
+		try
 		{
 			# 已经存在该仓库的文件夹
-			Set-Location $repoName
+			Push-Location $repoName
 			if ($branch_name)
 			{
 				git checkout $branch_name
 			}
-	
+
 			if ($LASTEXITCODE)
 			{
 				throw "没有该分支"
 			}
-	
+
 			git submodule update --init --recursive
 			git pull --recurse-submodules
 			if (-not $LASTEXITCODE)
@@ -32,35 +31,32 @@ try
 				# 如果上一个命令成功，则退出循环
 				return 
 			} 
+
+		}
+		finally
+		{
+			Pop-Location
+		}
+	}
+	else
+	{
+		# 不存在该仓库的文件夹，需要克隆
+		if ($branch_name)
+		{
+			git clone $git_url --recurse-submodules --branch $branch_name --depth 1
 		}
 		else
 		{
-			# 不存在该仓库的文件夹，需要克隆
-			if ($branch_name)
-			{
-				git clone $git_url --recurse-submodules --branch $branch_name --depth 1
-			}
-			else
-			{
-				git clone $git_url --recurse-submodules --depth 1
-			}
-	
-			if (-not $LASTEXITCODE)
-			{
-				# 如果上一个命令成功，则退出循环
-				return 
-			} 
+			git clone $git_url --recurse-submodules --depth 1
 		}
-	
-		Write-Host "克隆或拉取失败。将在 5 秒后重试。"
-		Start-Sleep -Seconds 5
+
+		if (-not $LASTEXITCODE)
+		{
+			# 如果上一个命令成功，则退出循环
+			return 
+		} 
 	}
-}
-catch
-{
-	throw
-}
-finally
-{
-	Pop-Location
+
+	Write-Host "克隆或拉取失败。将在 5 秒后重试。"
+	Start-Sleep -Seconds 5
 }
