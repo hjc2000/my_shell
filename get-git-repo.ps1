91051fbb4ee667ad2391eb856c6e9aec6b4d662e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-04 13:58:30 +0800

    auto commit

--- get-git-repo.ps1
+++ get-git-repo.ps1
@@ -2,6 +2,7 @@ param (
 	[Parameter(Mandatory = $true)]
 	[string]$git_url,
 	[string]$branch_name,
+	[switch]$recurse_submodules,
 	[switch]$all_depth
 )
 $ErrorActionPreference = "Stop"
@@ -15,14 +16,10 @@ while ($true)
 		{
 			# 已经存在该仓库的文件夹
 			Push-Location $repoName
-			if ($branch_name)
+			[string]$current_branch = git branch
+			if (-not $current_branch.Contains($branch_name))
 			{
-				git checkout $branch_name
-			}
-
-			if ($LASTEXITCODE)
-			{
-				throw "没有该分支"
+				throw "仓库文件夹已经存在，且分支名与 $branch_name 不符，先手动删除仓库文件夹，重新克隆。"
 			}
 
 			git submodule update --init --recursive
@@ -31,8 +28,7 @@ while ($true)
 			{
 				# 如果上一个命令成功，则退出循环
 				return 0
-			} 
-
+			}
 		}
 		finally
 		{
@@ -42,20 +38,26 @@ while ($true)
 	else
 	{
 		# 不存在该仓库的文件夹，需要克隆
-		$clone_cmd = "git clone $git_url --recurse-submodules"
+		$clone_cmd = "git clone $git_url"
+		if ($recurse_submodules)
+		{
+			$clone_cmd = "$clone_cmd --recurse-submodules"
+		}
+	
 		if ($branch_name)
 		{
 			$clone_cmd = "$clone_cmd --branch $branch_name"
 		}
-
+	
 		if (-not $all_depth)
 		{
 			$clone_cmd = "$clone_cmd --depth 1"
 		}
-
+		
 		Invoke-Expression $clone_cmd
 		if (-not $LASTEXITCODE)
 		{
+			# 克隆成功
 			return 0
 		} 
 	}
