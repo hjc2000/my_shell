c0cbf42f86ec90734098e56f1804dca5f7931b7d
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-08 09:51:38 +0800

    git-sync.ps1 支持传入提交记录的内容。

--- git-sync.ps1
+++ git-sync.ps1
@@ -4,6 +4,8 @@ param (
 $ErrorActionPreference = "Stop"
 
 Write-Host "正在进行同步的目录是：$(Get-Location)"
+Write-Host "本次的提交记录内容："
+Write-Host $commit_log
 git pull
 git add --all
 git commit -m $commit_log
