d66ea9b6fa1bf725f4c81f6c25bd6a64850be57d
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-08 09:53:27 +0800

    git-sync.ps1 提交时会打印提交记录的内容

--- git-sync.ps1
+++ git-sync.ps1
@@ -4,8 +4,14 @@ param (
 $ErrorActionPreference = "Stop"
 
 Write-Host "正在进行同步的目录是：$(Get-Location)"
+Write-Host "`n`n"
+Write-Host "-------------------------------------------------------------"
 Write-Host "本次的提交记录内容："
+Write-Host "-------------------------------------------------------------"
 Write-Host $commit_log
+Write-Host "-------------------------------------------------------------"
+Write-Host "`n`n"
+
 git pull
 git add --all
 git commit -m $commit_log
