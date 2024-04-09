78ebc652c4546b0492d70aef468979ee8f61d9fe
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-09 09:06:12 +0800

    auto commit

--- generate-importing-header.ps1
+++ generate-importing-header.ps1
@@ -16,11 +16,13 @@ foreach ($header_file in $header_files)
 {
 	if ($DoNotHoldTree)
 	{
+		# 不保持目录树。即 include 指令内不出现路径，只包含文件名。
 		$path = $header_file.Name
 		$importing_header_file_content += "#include<$path>`n"	
 	}
 	else
 	{
+		# 保持目录树。即 include 指令内的文件带有路径，相对于 $base_path。
 		$full_path = $header_file.FullName
 		$relative_ptah = get-relative-path.ps1 -BasePath $base_path -Path $full_path
 		$relative_ptah = $relative_ptah.Replace("\", "/").Replace("//", "/")
