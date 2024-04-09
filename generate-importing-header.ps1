8ccb36aa7842f07cede1c5e254868969b2cb0d13
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-09 09:24:37 +0800

    OutFilePath

--- generate-importing-header.ps1
+++ generate-importing-header.ps1
@@ -1,14 +1,14 @@
 param (
-	[string]$FileName = "include_all.h",
+	[string]$OutFilePath = "include_all.h",
 	[switch]$DoNotHoldTree,
-	# 开启此开关，结果仅会打印到控制台，不会创建文件并写入。此时 $FileName 参数会被忽略。
+	# 开启此开关，结果仅会打印到控制台，不会创建文件并写入。此时 $OutFilePath 参数会被忽略。
 	[switch]$WriteHostOnly
 )
 $ErrorActionPreference = "Stop"
 
-if ((Test-Path "./$FileName") -and (-not $WriteHostOnly))
+if ((Test-Path $OutFilePath) -and (-not $WriteHostOnly))
 {
-	throw "当前目录下已经存在 $FileName"
+	throw "$OutFilePath 已存在"
 }
 
 $base_path = Get-Location
@@ -27,7 +27,7 @@ foreach ($header_file in $header_files)
 		# 保持目录树。即 include 指令内的文件带有路径，相对于 $base_path。
 		$full_path = $header_file.FullName
 		$relative_path = get-relative-path.ps1 -BasePath $base_path -Path $full_path
-		$importing_header_file_content += "#include<$relative_path>`n"
+		$importing_header_file_content += "#include<$relative_path>`r`n"
 	}
 }
 
@@ -35,6 +35,6 @@ $importing_header_file_content = $importing_header_file_content.Trim()
 Write-Host $importing_header_file_content
 if (-not $WriteHostOnly)
 {
-	New-Item -Path "./$FileName" -ItemType File -Force
-	$importing_header_file_content | Set-Content -Path "./$FileName"
+	New-Item -Path $OutFilePath -ItemType File -Force
+	$importing_header_file_content | Set-Content -Path $OutFilePath
 }
