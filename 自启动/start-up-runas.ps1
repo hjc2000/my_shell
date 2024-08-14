81b3ee75b91e3b28b97b7abe8915ff22dccb4b35
hjc2000 <hjc.666@foxmail.com>
2024-08-14 08:34:31 +0800

    invoked-by-start-up-runas

--- 自启动/start-up-runas.ps1
+++ 自启动/start-up-runas.ps1
@@ -1,2 +1,2 @@
-# 将此文件放置到开机启动目录，然后在 bin 目录中放置一个 start-up.ps1 文件
-Start-Process pwsh -ArgumentList "-Command start-up.ps1" -Verb RunAs
+# 将此文件放置到开机启动目录，然后在 bin 目录中放置一个 invoked-by-start-up-runas.ps1 文件
+Start-Process pwsh -ArgumentList "-Command invoked-by-start-up-runas.ps1" -Verb RunAs
