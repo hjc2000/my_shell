230676d7a2a28964d6656470a62566dd06cf1f44
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-19 18:51:41 +0800

    ntp.ntsc.ac.cn

--- sync-time.ps1
+++ sync-time.ps1
@@ -1,4 +1,4 @@
 net start w32time
-w32tm /config /syncfromflags:manual /manualpeerlist:"time.windows.com" /update
+w32tm /config /syncfromflags:manual /manualpeerlist:"ntp.ntsc.ac.cn" /update
 w32tm /resync
 Set-TimeZone -Id "China Standard Time"
