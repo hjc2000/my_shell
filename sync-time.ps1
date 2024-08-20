eb4dc2087a7a6f18b2349aba480a99e7f62129ea
hjc2000 <hjc.666@foxmail.com>
2024-08-20 08:40:52 +0800

    while ($true)

--- sync-time.ps1
+++ sync-time.ps1
@@ -1,4 +1,12 @@
 net start w32time
 w32tm /config /syncfromflags:manual /manualpeerlist:"ntp.ntsc.ac.cn" /update
-w32tm /resync
+while ($true)
+{
+	w32tm /resync
+	if (-not $LASTEXITCODE)
+	{
+		break
+	}
+}
+
 Set-TimeZone -Id "China Standard Time"
