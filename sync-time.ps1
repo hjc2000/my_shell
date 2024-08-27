4e16be7f544f73b0ee12f9420ca1d890a5154b8e
hjc2000 <hjc.666@foxmail.com>
2024-08-27 08:47:47 +0800

    if ("$result".Contains("将 resync 命令发送到本地计算机 成功地执行了命令"))

--- sync-time.ps1
+++ sync-time.ps1
@@ -1,10 +1,11 @@
 net stop w32time
 net start w32time
-w32tm /config /syncfromflags:manual /manualpeerlist:"ntp.ntsc.ac.cn" /update
+w32tm /config /syncfromflags:manual /manualpeerlist:"ntp.ntsc.ac.cn,time.windows.com" /update
 while ($true)
 {
-	w32tm /resync
-	if (-not $LASTEXITCODE)
+	$result = w32tm /resync
+	Write-Host $result
+	if ("$result".Contains("将 resync 命令发送到本地计算机 成功地执行了命令"))
 	{
 		break
 	}
