3f5c53762b9624fff7426eedf7268d391597d13e
hjc2000 <hjc.666@foxmail.com>
2024-08-29 11:57:39 +0800

    if ($result.Contains("将 resync 命令发送到本地计算机 成功地执行了命令") -or 		$result.Contains("The command completed successfully")) 	{ 		break 	}

--- sync-time.ps1
+++ sync-time.ps1
@@ -5,7 +5,8 @@ while ($true)
 {
 	$result = w32tm /resync
 	Write-Host $result
-	if ("$result".Contains("将 resync 命令发送到本地计算机 成功地执行了命令"))
+	if ($result.Contains("将 resync 命令发送到本地计算机 成功地执行了命令") -or
+		$result.Contains("The command completed successfully"))
 	{
 		break
 	}
