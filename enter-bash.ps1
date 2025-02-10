9e76469d4762e3a917a03a28a71b5c240a6ba2fc
hjc2000 <hjc.666@foxmail.com>
2025-02-10 08:40:16 +0800

    $env:USERNAME

--- enter-bash.ps1
+++ enter-bash.ps1
@@ -7,9 +7,8 @@ $env:MSYSTEM_CHOST = "x86_64-w64-mingw32"
 $env:MSYSTEM_CARCH = "x86_64"
 $env:CHERE_INVOKING = "enabled_from_arguments"
 $env:CONTITLE = "MinGW UCRT x64"
-$env:HOME = "/home/huang"
-$env:USERNAME = "huang"
-$env:USER = "huang"
+$env:HOME = "/home/$env:USERNAME"
+$env:USER = "$env:USERNAME"
 $env:MINGW_PACKAGE_PREFIX = "mingw-w64-ucrt-x86_64"
 $env:PROMPT_COMMAND = 'history -a'
 
