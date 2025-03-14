7cfb790d292392dc3efb5dd997076f4a7e163bf4
hjc2000 <hjc.666@foxmail.com>
2025-03-14 14:42:05 +0800

    git-sync.ps1

--- link-cpp-project-config-file.ps1
+++ link-cpp-project-config-file.ps1
@@ -5,3 +5,5 @@ create-symbol-link.ps1 -Path ".clang-format" `
 try-remove-items.exe --paths "CMakePresets.json"
 create-symbol-link.ps1 -Path "CMakePresets.json" `
 	-Target "${env:cpp_lib_build_scripts_path}/工具链配置文件/C++项目根目录配置文件/CMakePresets.json"
+
+git-sync.ps1
