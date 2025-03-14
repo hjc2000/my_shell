cd048ffd6051e2993f78f355946ac3facc861ee6
hjc2000 <hjc.666@foxmail.com>
2025-03-14 14:45:54 +0800

    ../../../

--- link-cpp-project-config-file.ps1
+++ link-cpp-project-config-file.ps1
@@ -1,9 +1,9 @@
 try-remove-items.exe --paths ".clang-format"
 create-symbol-link.ps1 -Path ".clang-format" `
-	-Target "${env:cpp_lib_build_scripts_path}/工具链配置文件/C++项目根目录配置文件/.clang-format"
+	-Target "../../../工具链配置文件/C++项目根目录配置文件/.clang-format"
 
 try-remove-items.exe --paths "CMakePresets.json"
 create-symbol-link.ps1 -Path "CMakePresets.json" `
-	-Target "${env:cpp_lib_build_scripts_path}/工具链配置文件/C++项目根目录配置文件/CMakePresets.json"
+	-Target "../../../工具链配置文件/C++项目根目录配置文件/CMakePresets.json"
 
 git-sync.ps1
