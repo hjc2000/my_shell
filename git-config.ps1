02b0300528a6a4917aee3145ac6c94776eb11f84
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-23 17:05:05 +0800

    git-config

--- git-config.ps1
+++ git-config.ps1
@@ -1,4 +1,21 @@
-git config --global core.symlinks true
-git config --global core.longpaths true
-git config --global core.autocrlf false
-git config --global safe.directory '*'
+param (
+	[switch]$global
+)
+$ErrorActionPreference = "Stop"
+
+$global_option = ""
+if ($global)
+{
+	$global_option = "--global"
+}
+
+git config $global_option core.editor notepad
+git config $global_option core.symlinks true
+git config $global_option core.longpaths true
+git config $global_option core.autocrlf false
+git config $global_option core.ignorecase false
+
+git config $global_option safe.directory '*'
+git config $global_option http.sslverify false
+git config $global_option submodule.recurse true
+git config $global_option credential.helper store
