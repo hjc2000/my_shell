054e1800cc156dd219902ec709a52d0682ab7955
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-31 18:57:29 +0800

    pacman-ensure-packages

---
+++ pacman-ensure-packages.ps1
@@ -0,0 +1,24 @@
+param(
+	[Parameter(Mandatory = $true)]
+	[string[]]$RequiredPackages
+)
+
+foreach ($pkg in $RequiredPackages)
+{
+	Write-Output "检查包: $pkg"
+	run-bash-cmd.ps1 @"
+	pacman -Q $pkg
+"@
+
+	if ($LASTEXITCODE)
+	{
+		Write-Output "$pkg 还没被安装. 尝试安装："
+		run-bash-cmd.ps1 @"
+		pacman -S $pkg --noconfirm --overwrite '*'
+"@
+	}
+	else
+	{
+		Write-Output "$pkg 已经安装。"
+	}
+}
