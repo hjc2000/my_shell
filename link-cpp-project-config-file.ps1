b4d6ae4d4ee5ac9b148b496f900a162d968137cc
hjc2000 <hjc.666@foxmail.com>
2025-04-10 16:00:54 +0800

    删除create-symbol-link.ps1

--- link-cpp-project-config-file.ps1
+++ link-cpp-project-config-file.ps1
@@ -8,7 +8,8 @@ foreach ($file in $files)
 {
 	try-remove-items.exe --paths $file
 
-	create-symbol-link.ps1 -Path $file `
+	New-Item -ItemType SymbolicLink `
+		-Path $file `
 		-Target "../../../工具链配置文件/C++项目根目录配置文件/$file"
 }
 
