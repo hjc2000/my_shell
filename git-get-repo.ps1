0f26ad4bc021cf09247c6edc8ed3ea3e761d162d
hjc2000 <hjc.666@foxmail.com>
2025-07-07 11:37:54 +0800

    auto commit

--- git-get-repo.ps1
+++ git-get-repo.ps1
@@ -11,10 +11,10 @@ try
 	$repoName = git-parse-repo-name.ps1 -git_url $git_url
 	Push-Location $repoName
 
-	# 如果 $branch_name 参数制定了分支名，但是与当前分支名不符，要求重新克隆
+	# 如果 $branch_name 参数指定了分支名，但是与当前分支名不符，要求重新克隆
 	# 本脚本不具备签出到分支的能力。因为带有子模块时，签出到分支非常麻烦。
 	[string]$current_branch = git branch --show-current
-	if ($current_branch -cne $branch_name)
+	if (($branch_name -ne "") -and ($current_branch -cne $branch_name))
 	{
 		throw "仓库文件夹已经存在，且分支名与 $branch_name 不符。"
 	}
