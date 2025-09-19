e29241c757a7f35864d43270f0d9f84ca2aaa03e
hjc2000 <hjc.666@foxmail.com>
2025-09-19 16:06:32 +0800

    空行

--- pacman-ensure-packages.ps1
+++ pacman-ensure-packages.ps1
@@ -6,6 +6,7 @@ param(
 foreach ($pkg in $packages)
 {
 	Write-Output "检查包: $pkg"
+
 	run-bash-cmd.ps1 @"
 	pacman -Q $pkg
 "@
@@ -13,6 +14,7 @@ foreach ($pkg in $packages)
 	if ($LASTEXITCODE)
 	{
 		Write-Output "$pkg 还没被安装. 尝试安装："
+
 		run-bash-cmd.ps1 @"
 		pacman -S $pkg --noconfirm --overwrite '*'
 "@
