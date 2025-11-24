a05ae63f6a6d192357aeb5614c4c11ca6694c9de
hjc2000 <hjc.666@foxmail.com>
2025-11-24 10:29:03 +0800

    ``` feat(git-config): 添加 fetch.prune 配置以自动更新远程分支缓存
    在执行 git fetch 或 git pull 时，自动清理已删除的远程分支引用，
    避免本地缓存中残留无效的远程分支信息。
    ```

--- git-config.ps1
+++ git-config.ps1
@@ -15,6 +15,12 @@ git config $global_option core.longpaths true
 git config $global_option core.autocrlf false
 git config $global_option core.ignorecase false
 
+# 远程分支删除后，git 分支列表里还会显示远程有该分支，也就是不会从远程仓库重新
+# 获取分支列表更新本地的缓存信息。
+#
+# 设置了这个后，就可以自动更新缓存信息了。
+git config $global_option fetch.prune true
+
 git config $global_option safe.directory '*'
 git config $global_option http.sslverify false
 git config $global_option submodule.recurse true
