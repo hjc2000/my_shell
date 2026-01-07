7c41e3b32e2263bd3c10d0bf1810680db4396297
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-01-07 19:06:04 +0800

    ``` feat(git-pull): 增加上游分支跟踪检查
    当分支没有设置要跟踪的上游远程分支时，
    抛出相应的错误提示信息，避免拉取操作失败。
    ```

--- git-pull.ps1
+++ git-pull.ps1
@@ -9,6 +9,11 @@ while ($true)
 		throw "远程仓库不存在当前的分支，无法拉取。"
 	}
 
+	if ($output.Contains("git branch --set-upstream-to="))
+	{
+		throw "此分支还没有设置要跟踪的上游的远程分支。"
+	}
+
 	if (-not $LASTEXITCODE)
 	{
 		# 拉取成功
