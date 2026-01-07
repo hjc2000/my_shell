15dddc106fe6ca56dcb1cd2bac04eb893ef54896
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-01-07 19:22:13 +0800

    auto commit

--- git-pull.ps1
+++ git-pull.ps1
@@ -4,14 +4,16 @@ while ($true)
 	$output = git pull
 	$output = "$output"
 
+	git-check-whether-it-has-upstream.ps1
+
+	if ($LASTEXITCODE)
+	{
+		throw "此分支还没有设置要跟踪的上游的远程分支。"
+	}
+
 	if ($output.Contains("but no such ref was fetched"))
 	{
 		throw "远程仓库不存在当前的分支，无法拉取。"
-	}
-
-	if ($output.Contains("git branch --set-upstream-to="))
-	{
-		throw "此分支还没有设置要跟踪的上游的远程分支。"
 	}
 
 	if (-not $LASTEXITCODE)
